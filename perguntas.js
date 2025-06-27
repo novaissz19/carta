93cb83d23419c092c64d853615ba866ea21e6a01
novaissz19 <166837188+novaissz19@users.noreply.github.com>
2025-06-27 09:33:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'corpo humano'
+    'Quantos ossos o corpo humano tem na fase adulta?',
+    '206 ossos'
+)
+
+criaCartao(
+    'história',
+    'Em que ano começou a Segunda Guerra?',
+    '1939'
+)
+
+criaCartao(
+    'corpo humano',
+    'Qual é o maior órgão do corpo humano?',
+    'A pele'
+)
+
+criaCartao(
+    'corpo humano',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
